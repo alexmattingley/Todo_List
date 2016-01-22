413e1bd8750b25e10fd760612d198efef57747e9
Alex Mattingley <alexmattingely@gmail.com>
2016-01-21 18:56:40 -0800

    fixed it. YOU CAN ONLY PRINT JSON OBJECTS O SHIT WILL BREAK

--- data_handlers/data_handler_send.php
+++ data_handlers/data_handler_send.php
@@ -20,8 +20,7 @@ if(!empty($_SESSION['user_id'])){
     $results = mysqli_query($conn, $query);
     if(mysqli_affected_rows($conn) > 0){
         $json_for_return = json_encode($_POST["task_data"]);
-        print('json for return' . $json_for_return);
-        print_r('$results ' . $results);
+        print($json_for_return);
     }else {
         print('something is not working, its probably the id breaking things again. Check out the query below.');
         print($query);
